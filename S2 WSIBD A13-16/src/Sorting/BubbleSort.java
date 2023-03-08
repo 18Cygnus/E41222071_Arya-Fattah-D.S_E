d6ef79a84d9898dea7d53e6975fb4347d9d721f3
18Cygnus <aryasanjaya001@gmail.com>
2023-03-08 09:00:53 +0700

    Week 4

---
+++ S2 WSIBD A13-16/src/Sorting/BubbleSort.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Sorting;
+
+/**
+ *
+ * @author aryas
+ */
+public class BubbleSort {
+    static void bubblesort(int[] arr){
+        int n = arr.length;
+        int temp = 0;
+        for(int i=0; i<n; i++){
+            for(int j=1; j<(n-i); j++){
+                if(arr[j-1] > arr[j]){
+                    //swap elements
+                    temp = arr[j-1];
+                    arr[j-1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+    }
+    
+    public static void main(String[] args){
+        int arr[] = {3,60,35,2,45,320,5};
+        
+        System.out.println("Array Before Bubble Sort");
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i]+" ");
+        }
+            System.out.println();
+            
+            bubblesort(arr); //sorting array elements using bubble sort
+            
+            System.out.println("Array After Bubble Sort");
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
---
+++ S2 WSIBD A13-16/src/s2/wsibd/a13/pkg16/S2WSIBDA1316.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package s2.wsibd.a13.pkg16;
+
+/**
+ *
+ * @author aryas
+ */
+public class S2WSIBDA1316 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
